999929cf2ebde4b511f3318dd462833373959ef9
Timothy <tcraig258@gmail.com>
2024-12-02 09:06:50 +0100

    added backlash correction

--- tomobase/processes/alignments/rotation.py
+++ tomobase/processes/alignments/rotation.py
@@ -4,10 +4,15 @@ from copy import copy
 import napari
 
 from scipy.ndimage import center_of_mass, shift, rotate
+from scipy.optimize import minimize_scalar
+
 
 from tomobase.hooks import tomobase_hook_process
 from tomobase.registrations.transforms import TOMOBASE_TRANSFORM_CATEGORIES
 from tomobase.data import Sinogram
+from tomobase.processes.reconstruct import reconstruct
+from tomobase.processes.forward_project import project
+from tomobase.log import logger
 
 from qtpy.QtWidgets import QWidget, QComboBox, QLabel, QSpinBox, QHBoxLayout, QLineEdit, QVBoxLayout, QPushButton, QGridLayout, QDoubleSpinBox
 from qtpy.QtCore import Qt
@@ -75,60 +80,33 @@ def align_tilt_axis_shift(sino: Sinogram, method='sirt', offsets=None, offset=No
     
     
 @tomobase_hook_process(name='Align Tilt Rotation', category=TOMOBASE_TRANSFORM_CATEGORIES.ALIGN.value(), subcategories=_subcategories)
-def align_tilt_axis_rotation(sino: Sinogram, method='sirt', angles=None, angle=None,
-                             inplace=True, verbose=True, return_angle=False, **kwargs):
-    """Align the rotation of the tilt axis of a sinogram other using
-    reprojection
-
-    To apply this algorithm, the sinogram must already be roughly aligned, with
-    ``align_sinogram_center_of_mass`` and ``align_tilt_axis_shift`` for example.
-    Multiple iterations of this method might be required for optimal alignment.
-
-    Arguments:
-        sino (Sinogram)
-            The projection data
-        method (str)
-            The reconstruction algorithm (default: 'sirt')
-        angles (np.ndarray)
-            A list of angles to try in degrees, if None is given it will use
-            ``numpy.arange(-4, 5)`` (default: None)
-        angle (float)
-            A pre-calculated angle in degrees, this is useful for aligning
-            multiple sinograms simultaneously (default: None)
-        inplace (bool)
-            Whether to do the alignment in-place in the input data object
-            (default: True)
-        verbose (bool)
-            Display a progress bar if applicable (default: True)
-        return_angle (bool)
-            If True, the return value will be a tuple with the angle in the
-            second item (default: False)
-        kwargs (dict)
-            Other keyword arguments are passed to ``reconstruct``
-
-    Returns:
-        Sinogram
-            The result
-    """
+def backlash_correct(sino: Sinogram, tolerance= 10, method='bounded', extend_return=False, inplace = True):
+    """Correct Backlash Artefacts by Curve Fitting """
+    
     if not inplace:
         sino = copy(sino)
+        
+    def objective_function(value, sino, indices):
+        angles = copy(sino.angles)
+        sino.angles[indices] += value
+        reproj = project(reconstruct(sino, 'fbp'), sino.angles[indices])        
+        error = np.sqrt(np.mean((sino.data[:,:, indices] - reproj.data) ** 2))
+        sino.angles = angles
+        logger.debug(f'Error: {error}')
+        return  error
+    
+    indices = np.where(np.diff(sino.angles) < 0)[0] + 1
+    value = 0
 
-    if angle is None:
-        if angles is None:
-            angles = np.arange(-4, 5)
-        mse = np.zeros(len(angles))
-        sino_rot = copy(sino)
-        for i in range(len(angles)):
-            sino_rot.data = rotate(sino.data, angles[i], reshape=False)
-            reproj = project(reconstruct(sino_rot, method, **kwargs),
-                            sino.angles)
-            mse[i] = np.mean((sino_rot.data - reproj.data) ** 2)
-
-        angle = angles[np.argmin(mse)]
-
-    sino.data = rotate(sino.data, angle, reshape=False)
-
-    if return_angle:
-        return sino, angle
+    if method == 'bounded':
+        result = minimize_scalar(objective_function, value, args=(sino, indices), bounds=(-tolerance, +tolerance), method=method)
+    else:
+        result = minimize_scalar(objective_function, value, args=(sino, indices), method=method)
+        
+    sino.angles[indices] += result.x
+    # Summarize Fit
+    logger.debug(f'Final Error: {result.fun}, Angle Shift: {result.x}')
+    if extend_return:
+        return sino, result.x
     else:
         return sino
