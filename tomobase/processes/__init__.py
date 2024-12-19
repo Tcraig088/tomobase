1201121d3d4cfa6c569c6269ee9d62f33388a11d
Timothy <tcraig258@gmail.com>
2024-12-19 13:02:04 +0100

    added -GRS weghted angular correction

--- tomobase/processes/__init__.py
+++ tomobase/processes/__init__.py
@@ -1,2 +1,2 @@
 from tomobase.processes.forward_project import project
-from tomobase.processes.reconstruct import reconstruct
+from tomobase.processes.reconstruct import reconstruct, reconstruct_weighted_sirt
--- tomobase/processes/reconstruct.py
+++ tomobase/processes/reconstruct.py
@@ -10,6 +10,98 @@ from tomobase.registrations.transforms import TOMOBASE_TRANSFORM_CATEGORIES
 from tomobase.log import logger
 
 
+
+@tomobase_hook_process(name='Reconstruct Weighted SIRT', category=TOMOBASE_TRANSFORM_CATEGORIES.RECONSTRUCT.value())
+def reconstruct_weighted_sirt(sino:Sinogram, iterations:int=0, use_gpu:bool=True, weighted=False):
+    """Reconstruct a volume from a given sinogram.
+
+    Arguments:
+        sinogram (Sinogram)
+            The projection data
+        method (str)
+            The reconstruction algorithm; supported algorithms are: `'bp'`,
+            `'fbp'`, `'sirt'`, `'em'`, `'sart'` and `'cgls'`
+        iterations (int)
+            The number of iterations when using an iterative reconstructor,
+            leaving this at None will select the default number of iterations
+            for the given algorithm (default: None)
+        use_gpu (bool)
+            Use a GPU if it is available (default: True)
+        verbose (bool)
+            Display a progress bar if applicable (default: True)
+        mask (numpy.ndarray)
+            Boolean mask that indicates which voxels should be used in the
+            reconstruction (default: None)
+
+    Returns:
+        Volume
+            The reconstructed volume
+    """
+    indices = np.argsort(sino.angles)
+    sino.angles = sino.angles[indices]
+    data = sino.data[:,:,indices]
+    data = np.transpose(data, (0, 2, 1))  # ASTRA expects (z, n, d)
+
+    weights= np.ones_like(sino.angles)
+    if weighted == True:
+        sorted_angles = deepcopy(sino.angles) +  90
+        n_angles = len(sorted_angles)
+        print(n_angles)
+        for i in range(len(sorted_angles)):
+            if i == 0:
+                weights[i] = 0.5*(180 - sorted_angles[n_angles-1] + sorted_angles[i+1])
+            elif i == len(sorted_angles)-1:
+                weights[i] = 0.5*(180 - sorted_angles[n_angles-2] + sorted_angles[0])
+            else:
+                weights[i] = 0.5*((sorted_angles[i+1] - sorted_angles[i]) + (sorted_angles[i] - sorted_angles[i-1]))
+        ratio = 180/(n_angles-1)
+        weights = weights/ratio
+    
+    
+    use_gpu = use_gpu and astra.use_cuda()
+
+    if iterations is None:
+        iterations = _get_default_iterations('sirt')
+
+    z, n, d = data.shape
+    proj_id = _create_projector(d, d, sino.angles, use_gpu)
+
+    iterator = range(z)
+
+    vol = np.zeros((z, d, d))
+    default_mask = _circle_mask(d)
+
+    mask = None
+    if mask is None:
+        mask = np.ones((z, d, d), dtype=bool)
+    else:
+        mask = np.transpose(mask, (2, 1, 0))  # ASTRA expects (z, y, x)
+
+    W = astra.OpTomo(proj_id)
+    domain_shape = np.ones((d, d))
+    range_shape = np.ones((n, d))
+    R = np.reshape(1/(W*domain_shape), (n, d))
+    C = np.reshape(1/(W.T*range_shape), (d, d))
+    R = np.minimum(R, 1 / 10**-6)
+    C = np.minimum(C, 1 / 10**-6)
+
+    for i in iterator:
+        for j in range(iterations):
+            A = np.transpose(np.transpose(np.reshape(W*vol[i, :, :], (n, d)), (1,0))*weights,(1,0)) 
+            B = np.transpose(np.reshape(np.transpose(data[i, :, :],(1,0))*weights,(d,n)), (1,0))
+            D = R*(B - A)
+            vol[i, :, :] += C*np.reshape(W.T*D,(d,d))
+            vol[i, :, :] = np.reshape(np.minimum(vol[i, :, :], data.max()), (d, d))
+            vol[i, :, :] = np.reshape(np.maximum(vol[i, :, :], 0), (d, d))
+            vol[i, :, :] = vol[i, :, :] * (default_mask & mask[i, :, :])
+
+
+    volume = Volume(np.transpose(vol, (2, 1, 0)), sino.pixelsize)  # ASTRA gives (z, y, x)
+    astra.astra.delete(proj_id)
+    logger.info('type of volume: ' + str(type(volume)))
+    return volume
+
+
 @tomobase_hook_process(name='Reconstruct', category=TOMOBASE_TRANSFORM_CATEGORIES.RECONSTRUCT.value())
 def reconstruct(sino:Sinogram, method:str='sirt', iterations:int=0, use_gpu:bool=True):
     """Reconstruct a volume from a given sinogram.
